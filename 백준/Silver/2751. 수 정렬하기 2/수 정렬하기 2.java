1a9d5b67277b74bc7be7e1b15318c1f09e715b7d
한우석 <106640954+drunkenhw@users.noreply.github.com>
2023-09-08 19:28:06 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2944 ms, Memory: 324660 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        Arrays.sort(arr);  
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i]).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
