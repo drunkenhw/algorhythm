b9e5758fbc844c50c1506659b401001bb524fb74
한우석 <106640954+drunkenhw@users.noreply.github.com>
2023-09-19 19:45:03 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 224 ms, Memory: 17852 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,30 +1,26 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        int[][] a = new int[15][15];
-
-        for(int i = 0; i < 15; i++) {
-            a[i][1] = 1;	
-            a[0][i] = i;	
+        int n = scanner.nextInt();
+        int[][] apart = new int[15][15];
+        for (int i = 0; i < 15; i++) {
+            apart[i][1] = 1;
+            apart[0][i] = i;
         }
 
-
-        for(int i = 1; i < 15; i ++) {
-            for(int j = 2; j < 15; j++) {	
-                a[i][j] = a[i][j - 1] + a[i - 1][j];
+        for (int i = 1; i < 15; i++) {
+            for (int j = 2; j < 15; j++) {
+                apart[i][j] = apart[i][j - 1] + apart[i - 1][j];
             }
         }
-
-        int count = scanner.nextInt();
-
-        for(int i = 0; i < count; i++) {
-            int k = scanner.nextInt();
-            int n = scanner.nextInt();
-            System.out.println(a[k][n]);
+        for (int i = 0; i < n; i++) {
+            int floor = scanner.nextInt();
+            int ho = scanner.nextInt();
+            System.out.println(apart[floor][ho]);
         }
     }
 }
